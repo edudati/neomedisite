36f04cb69313cdfa43106237fe83ce46a1031053
edudati <edudati@gmail.com>
2025-07-02 09:43:00 -0300

    update signup method

--- noemedi-site/src/Signup.jsx
+++ noemedi-site/src/Signup.jsx
@@ -25,7 +25,7 @@ function Signup() {
 
       const token = await user.getIdToken();
 
-      const response = await fetch("http://localhost:8000/api/users", {
+      const response = await fetch("http://127.0.0.1:8000/api/v1/users", {
         method: "POST",
         headers: {
           "Authorization": `Bearer ${token}`,
